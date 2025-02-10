870ce9e29e23a86971255987834600f2c4dfc484
Cruzjonathan22 <cruzjonathan_bsit@plmun.edu.ph>
2025-02-10 15:15:32 +0800

    "My first Python program"

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("Hello world!")
-print("Name: [Jonathan Arciaga Cruz]")
-print("Course: [Bachelor of Science in Information Technology]")
-print("Interests in programming: [Everything]")
+print("Name: [Your Full Name]")
+print("Course: [Your Course Name]")
+print("Interests in programming: [Your Interests]")
